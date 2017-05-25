85e178f5ace2e738404813858b8ce8b908e4addb
Anatoliy Klots and Eric Schoettle <anatoliy@>
2017-05-25 09:17:34 -0700

    finished

--- js/bike-interface.js
+++ js/bike-interface.js
@@ -21,13 +21,41 @@ var BikeSearch = require('./../js/bike-search.js').bikeSearchModule;
 var displaySearchResults = function(searchObject, response) {
   for (var i = 0; i < response.length; i++) {
     if (response[i].title) {
-      $('#output').append(response[i].title)
+      $('#output').append(concatBikeData(response[i]))
     } else {
       $('#output').append(item)
     }
   }
 }
 
+
+var concatBikeData = function(bikeData) {
+  string = '<div class="col-md-3">'
+  if (bikeData.title) {
+    string = string + '<h3>' + bikeData.title + '</h3>'
+  }
+  if (bikeData.thumb) {
+    string = string + '<img src="' + bikeData.thumb + '" alt="bike_img_missing" />'
+  }
+  if (bikeData.frame_colors || bikeData.brand || bikeData.model) {
+    string = string + '<h4>'
+    if (bikeData.frame_colors) {
+      bikeData.frame_colors.forEach(function(color){
+        string = string + color + ", "
+      })
+    }
+    if (bikeData.brand) {
+      string = string + bikeData.brand + ", "
+    }
+    if (bikeData.model) {
+      string = string + bikeData.model + ", "
+    }
+    string = string.slice(0, -2);
+    string = string + '</h4>';
+  }
+  string = string + '</div>';
+  return string;
+}
 //second
 $(document).ready(function() {
   $('#search').click(function() {
